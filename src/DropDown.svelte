4cef9b52400f6fe7924659f6e892c86ecbd97f69
Deciel <shongo021@gmail.com>
2025-05-06 17:08:24 +0200

    Started adding props and basic functionality

--- src/DropDown.svelte
+++ src/DropDown.svelte
@@ -1,1 +1,26 @@
-<div class="mt-36 h-12 w-3xl rounded-md border-2">Dropdown :D</div>
+<script lang="ts">
+	let { placeholder, options } = $props();
+	let selected = $state(placeholder);
+
+	function selectOption(option: string) {
+		selected = option;
+	}
+</script>
+
+<button class="mt-36 flex h-12 w-3xl flex-row items-center justify-between rounded-md border-2">
+	{selected}
+</button>
+<ul>
+	{#each options as option}
+		<li>
+			<button
+				class="cursor-pointer"
+				onclick={() => {
+					selectOption(option);
+				}}
+			>
+				{option}
+			</button>
+		</li>
+	{/each}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import DropDown from '../DropDown.svelte';
+	let options = ['Option 1', 'Option 2', 'Option 3'];
+	let placeholder = 'Select option';
 </script>
 
 <main class="flex h-screen w-full flex-col items-center">
 	<h1 class="mt-5 text-5xl font-semibold">Frontend Challenge #1</h1>
-	<DropDown />
+	<DropDown {placeholder} {options} />
 </main>
