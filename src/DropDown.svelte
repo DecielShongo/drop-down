0c873324a747680c731fa43493917bf0b65d6453
Deciel <shongo021@gmail.com>
2025-05-12 11:07:38 +0200

    Added Enter functionality

--- src/DropDown.svelte
+++ src/DropDown.svelte
@@ -1,29 +1,36 @@
 <script lang="ts">
 	let { placeholder, options } = $props();
 	let selected = $state(placeholder);
+	let selectedTmp = $state(placeholder);
 	let index = $state(options.length);
 
 	function selectOption(option: string) {
 		selected = option;
+		selectedTmp = option;
 	}
-	function handleArrowKeys(e: KeyboardEvent) {
-		if (index != options.length || e.code == 'ArrowUp') {
-			e.code == 'ArrowDown' && (index = (index + 1) % options.length);
-			e.code == 'ArrowUp' && (index = (index - 1) % options.length);
-		} else {
-			index = 0;
-		}
-		if (index < 0) {
-			selected = options[options.length + index];
-		} else {
-			selected = options[index];
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.code == 'ArrowDown' || e.code == 'ArrowUp') {
+			if (index != options.length || e.code == 'ArrowUp') {
+				e.code == 'ArrowDown' && (index = (index + 1) % options.length);
+				e.code == 'ArrowUp' && (index = (index - 1) % options.length);
+			} else {
+				index = 0;
+			}
+			if (index < 0) {
+				selectedTmp = options[options.length + index];
+			} else {
+				selectedTmp = options[index];
+			}
+		} else if (e.code == 'Enter') {
+			console.log('Enter');
+			selectOption(selectedTmp);
 		}
 	}
 </script>
 
 <button
 	class="mt-36 flex h-12 w-3xl flex-row items-center justify-between rounded-md border-2"
-	onkeydown={(e) => (e.code == 'ArrowDown' || e.code == 'ArrowUp') && handleArrowKeys(e)}
+	onkeydown={(e) => handleKeyDown(e)}
 >
 	{selected}
 </button>
@@ -32,10 +39,12 @@
 		<li>
 			<button
 				class="cursor-pointer"
+				class:border-2={option == selectedTmp}
 				onclick={() => {
 					selectOption(option);
 					index = options.indexOf(option);
 				}}
+				onkeydown={(e) => handleKeyDown(e)}
 			>
 				{option}
 			</button>
